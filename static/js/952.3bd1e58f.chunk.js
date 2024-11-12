378fb519131df1574e7b55d410a1d7253dd48875
Michal Jeřábek <michal.jerabek@bluesoft.cz>
2024-11-12 20:49:16 +0100

    Updates

---
+++ static/js/952.3bd1e58f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkslovanaci=self.webpackChunkslovanaci||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.3bd1e58f.chunk.js.map
