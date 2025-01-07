3b87eb2cd913cde81a54132c26e99298cd42d441
Michal Jeřábek <michal.jerabek@bluesoft.cz>
2025-01-08 00:54:07 +0100

    remove starting text

--- slovanaci/src/components/Home.js
+++ slovanaci/src/components/Home.js
@@ -5,9 +5,6 @@ const Home = () => {
     return (
       <div className='players-container'>
         <h1>Slovaňáci</h1>
-        <span>
-          Jednoduchý web pro zaznamenávání našich výsledků. Příjímám všechny návrhhy na úpravu :)
-        </span>
         <LastMatch />
       </div>
     );
